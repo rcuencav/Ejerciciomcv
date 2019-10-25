6e1b19d5f92bc71894322728030ed5087a6a510f
rcuencav <40639734+rcuencav@users.noreply.github.com>
2019-10-25 17:51:25 -0500

    Add files via upload
    Apertura Base de Aplicacion MVC

---
+++ aplicacionmvc/aplicacionmvc/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using aplicacionmvc.Models;
+
+namespace aplicacionmvc.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public Contact contact { set; get; }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+       
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+      
+        public IActionResult RegisterContact()
+        {
+
+            ViewData["Message"] = "Exitoso";
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RegisterContact(Contact C)
+        {
+
+            ViewData["Message"] = "Exitoso";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ aplicacionmvc/aplicacionmvc/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aplicacionmvc.Models
+{
+    public class Contact
+    {
+        public string nombres { set; get; }
+        public string apellidos { set; get; }
+        public int edad { set; get; }
+        public string correo { set; get; }
+        public string direccion { set; get; }
+    }
+}
